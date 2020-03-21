7975b2ca304eb25fbd9b34593c78fc096aadb85c
mustaque <hussainmustaque98407@gmail.com>
2020-03-21 19:04:33 +0530

    modify ui

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,11 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPostData = (req, res) => {
-    res.render('index', { title: 'Home' });
+    Post.find()
+        .then(post => {
+            console.log(post);
+            res.render('index', { title: 'Home', post: post });
+        })
+        .catch(err => console.log(err));
 };
